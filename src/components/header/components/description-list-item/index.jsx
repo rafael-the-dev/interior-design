a7dd503c9f130488d009026bf74722a0dc2d4086
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-24 11:30:55 +0200

    finish description list responsiveness

--- src/components/header/components/description-list-item/index.jsx
+++ src/components/header/components/description-list-item/index.jsx
@@ -5,10 +5,10 @@ const DescriptionListItem = ({ description, icon, title }) => {
 
     return (
         <div className={classNames(icon, "description-list__item flex flex-col items-center mb-6 relative",
-        "before:absolute last:mb-0 hover:before:text-red-600")}>
+        "before:absolute last:mb-0 before:text-gray-400 hover:before:text-red-600")}>
             <Typography
                 component="dt"
-                className="font-bold text-lg">
+                className="font-bold text-lg sm:text-xl">
                 { title }
             </Typography>
             <Typography
@@ -21,13 +21,59 @@ const DescriptionListItem = ({ description, icon, title }) => {
                     `
                         .description-list__item::before {
                             font-size: 1.8rem;
-                            left: 1rem;
+                            left: 10%;
                             top: 50%;
                             transform: translateY(-50%);
                         }
 
                         .description-list__item {
                             padding-left: 2rem;
+                        }
+
+                        @media screen and (min-width: 420px) {
+                            .description-list__item::before {
+                                font-size: 2.5rem;
+                                left: 20%;
+                            }
+                        }
+
+                        @media screen and (min-width: 550px) {
+                            .description-list__item::before {
+                                font-size: 2.8rem;
+                                left: 22%;
+                            }
+                        }
+
+                        @media screen and (min-width: 768px) {
+                            .description-list__item::before {
+                                left: 28%;
+                            }
+                        }
+
+                        @media screen and (min-width: 990px) {
+                            .description-list__item::before {
+                                font-size: 2.8rem;
+                                left: 0;
+                            }
+
+                            .description-list__item {
+                                align-items: flex-start;
+                                margin-bottom: 0;
+                                padding-left: 4rem;
+                                width: calc(100% / 3);
+                            }
+                        }
+
+                        @media screen and (min-width: 1100px) {
+                            .description-list__item {
+                                width: calc((100% / 3) - 1%);
+                            }
+                        }
+
+                        @media screen and (min-width: 1200px) {
+                            .description-list__item {
+                                padding: 0 5%;                             
+                            }
                         }
                     `
                 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,10 @@
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import classNames from "classnames";
 
 import classes from "./styles.module.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 import Link from "../link";
 import DescriptionListItem from "./components/description-list-item";
@@ -18,23 +21,43 @@ const Header = () => {
                 </Typography>
             </div>
             <Menu />
-            <dl className="bg-gray-100 pt-16 pb-10 px-5 sm:px-[10%]">
-                <DescriptionListItem  
-                    description="Flat 201, Reynolds Neck Str"
-                    icon="icon-maps-and-location"
-                    title="Newyork, USA"
-                />
-                <DescriptionListItem  
-                    description="Mon - Friday: 9.00 to 18.00"
-                    icon="icon-phone"
-                    title="+324 123 45 978"
-                />
-                <DescriptionListItem  
-                    description="Get a Free Quote"
-                    icon="icon-mail"
-                    title="Support@Mailus.Com"
-                />
-            </dl>
+            <div className={classNames(classes.descriptionListContainer, "bg-gray-100 pt-16 pb-10 px-5 sm:px-[10%] remove-px-10",
+                "flex flex-col items-stretch")}>
+                <dl className={classNames(classes.descriptionList, "")}>
+                    <DescriptionListItem  
+                        description="Flat 201, Reynolds Neck Str"
+                        icon="icon-maps-and-location"
+                        title="Newyork, USA"
+                    />
+                    <DescriptionListItem  
+                        description="Mon - Friday: 9.00 to 18.00"
+                        icon="icon-phone"
+                        title="+324 123 45 978"
+                    />
+                    <DescriptionListItem  
+                        description="Get a Free Quote"
+                        icon="icon-mail"
+                        title="Support@Mailus.Com"
+                    />
+                </dl>
+                <ul className={classNames(classes.linksList, "flex justify-center mt-6")}>
+                    <li className="mr-3">
+                        <IconButton className="bg-white p-3">
+                            <FacebookIcon />
+                        </IconButton>
+                    </li>
+                    <li className="mr-3">
+                        <IconButton className="bg-white p-3">
+                            <LinkedInIcon />
+                        </IconButton>
+                    </li>
+                    <li className="">
+                        <IconButton className="bg-white p-3">
+                            <InstagramIcon />
+                        </IconButton>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 };
