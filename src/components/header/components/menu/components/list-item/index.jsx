bf013d2cf9d72db92c1f0c4d0ac6ff2800d4ac7f
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-29 14:20:17 +0200

    close open menu when opening new one

--- src/components/header/components/menu/components/list-item/index.jsx
+++ src/components/header/components/menu/components/list-item/index.jsx
@@ -1,5 +1,6 @@
-import { useCallback, useEffect, useMemo, useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import { Button, Collapse, Typography } from "@mui/material";
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 import { useRouter } from "next/router"
 import classNames from "classnames";
 
@@ -30,47 +31,31 @@ const ListItem = ({ id, list, label }) => {
         </>
     ), [ list ])
 
-    const toggleState = useCallback(() => setOpen(b => !b), []);
-
-    const clickHandler = useCallback((event) => {
-        if(!event.target.getAttribute("data-navigation")) {
-            setOpen(false);
-        }
-    }, [])
-
-    useEffect(() => {
-        const currentWindow = window;
-
-        if(open) {
-            currentWindow.addEventListener("click", clickHandler);
-        } else {
-            currentWindow.removeEventListener("click", clickHandler);
-        }
-
-        return () => {
-            currentWindow.removeEventListener("click", clickHandler);
-        };
-    }, [ clickHandler, open ])
+    const toggleState = useCallback((event) => setOpen(b => !b), []);
+    const closeHandler = useCallback(() => setOpen(false), []);
 
     return (
-        <li className={classNames(classes.listItem, "border-b border-solid border-neutral-800")}>
-            <Button 
-                className={classNames("items-center justify-between py-3 px-5 w-full hover:text-red-600",
-                classes.listItemButton, pathname === id ? classes.currentPage : "text-white")}
-                data-navigation="item"
-                endIcon={ open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                onClick={toggleState}>
-                { label }
-            </Button>
-            <Collapse className={classes.listItemCollapse} in={open} unmountOnExit>
-                <ul className="flex flex-col px-5">{ listMemo }</ul>
-            </Collapse>
-            <ul 
-                className={classNames(classes.listItemList, "absolute bg-white flex flex-col h-0 overflow-hidden px-5",
-                { [classes.listItemListVisible]: open })}>
-                { listMemo }
-            </ul>
-        </li>
+        <ClickAwayListener
+            onClickAway={closeHandler}>
+            <li className={classNames(classes.listItem, "border-b border-solid border-neutral-800")}>
+                <Button 
+                    className={classNames("items-center justify-between py-3 px-5 w-full hover:text-red-600",
+                    classes.listItemButton, pathname === id ? classes.currentPage : "text-white")}
+                    data-navigation="button"
+                    endIcon={ open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    onClick={toggleState}>
+                    { label }
+                </Button>
+                <Collapse className={classes.listItemCollapse} in={open} unmountOnExit>
+                    <ul className="flex flex-col px-5">{ listMemo }</ul>
+                </Collapse>
+                <ul 
+                    className={classNames(classes.listItemList, "absolute bg-white flex flex-col h-0 overflow-hidden px-5",
+                    { [classes.listItemListVisible]: open })}>
+                    { listMemo }
+                </ul>
+            </li>
+        </ClickAwayListener>
     );
 };
 
