31f1d06bf2a69e4e635cea9bc8d51a04e3b5a872
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-30 17:50:50 +0200

    create single fact counter card

---
+++ src/components/about-page/single-fact-counter/index.jsx
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material";
+import classNames from "classnames";
+
+import classes from "./styles.module.css"
+
+const Container = ({ description, title }) => (
+    <Typography className={classNames(classes.container, `flex font-semibold items-center mb-4 relative
+         last:mb-0`)}>
+        <div className={classNames(classes.titleContainer, "mr-4 relative before:absolute before:bg-white before:h-full before:w-full")}>
+            <Typography
+                component="span"
+                className="relative text-5xl z-10">
+                { title }
+            </Typography>
+        </div>
+        <Typography
+            component="span"
+            className={classNames("text-lg")}>
+            { description }
+        </Typography>
+    </Typography>
+);
+
+export default Container;
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -4,8 +4,8 @@ import { Typography } from "@mui/material";
 import classes from "./styles.module.css"
 
 import DefaultHero from "src/components/default-hero";
+import SingleFactCounter from "src/components/about-page/single-fact-counter"
 import ModernDesignCarolusel from "src/components/about-page/modern-design"
-
 
 const Container = () => {
 
@@ -19,23 +19,32 @@ const Container = () => {
                     Talented interior<br/>designer.
                 </Typography>
             </DefaultHero>
-            <section className="px-5 py-12 sm:px-[10%] lg:px-5">
-                <div className="items-center justify-between lg:flex ">
-                    <Typography 
-                        component="h2"
-                        className={classNames(classes.modernDesignTitle, `
-                        font-medium text-2xl uppercase md:text-3xl lg:text-4xl before:block before:font-bold before:mb-2 before:text-red-500`)}>
-                        <span className="font-bold">Modern &amp; Luxury</span><br/>
-                        Interior designers in USA
-                    </Typography>
-                    <Typography
-                        className={classNames(classes.moderDesignDescription, "leading-6 mt-4 text-gray-600 text-sm md:text-base")}>
-                        We feel that interior design is one of the most important investments you will 
-                        ever make. Whether your needs are Residential or Commercial Interior Design or 
-                        Model Home Merchandising, our commitment to your project.
-                    </Typography>
+            <section className="py-12">
+                <div className="px-5 sm:px-[10%] lg:px-5">
+                    <div className="items-center justify-between lg:flex ">
+                        <Typography 
+                            component="h2"
+                            className={classNames(classes.modernDesignTitle, `
+                            font-medium text-2xl uppercase md:text-3xl lg:text-4xl before:block before:font-bold before:mb-2 before:text-red-500`)}>
+                            <span className="font-bold">Modern &amp; Luxury</span><br/>
+                            Interior designers in USA
+                        </Typography>
+                        <Typography
+                            className={classNames(classes.moderDesignDescription, "leading-6 mt-4 text-gray-600 text-sm md:text-base")}>
+                            We feel that interior design is one of the most important investments you will 
+                            ever make. Whether your needs are Residential or Commercial Interior Design or 
+                            Model Home Merchandising, our commitment to your project.
+                        </Typography>
+                    </div>
+                    <ModernDesignCarolusel />
                 </div>
-                <ModernDesignCarolusel />
+                <div className={classNames(classes.SingleFactCounterContainer, 
+                    "bg-gray-100 flex flex-col justify-center items-stretch px-[20%] lg:px-5")}>
+                    <SingleFactCounter description={<>Years of<br/>Experience</>} title="36" />
+                    <SingleFactCounter description={<>Projects<br/>Completed</>} title="5k" />
+                    <SingleFactCounter description={<>Branches<br/>Worldwide</>} title="24" />
+                    <SingleFactCounter description={<>Awards<br/>Holds in Hand</>} title="47" />
+                </div>
             </section>
         </main>
     );
