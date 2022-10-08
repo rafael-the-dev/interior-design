175b29e8796afb0f6d0afcd2014833e43cbe1b69
Rafael Tivane <luislangabusiness@gmail.com>
2022-10-08 07:25:47 +0200

    create hover animation to card

--- src/components/projects-page/card/index.jsx
+++ src/components/projects-page/card/index.jsx
@@ -1,20 +1,40 @@
 import Image from "next/image";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import classNames from "classnames";
 
 import classes from "./styles.module.css";
 
+import LaunchIcon from '@mui/icons-material/Launch';
+
 const ListItem = ({ category, image, selectedTab, title }) => {
-    console.log(category, selectedTab, !category.includes(selectedTab),  selectedTab !== "all")
+
     return (
-        <li className={classNames(classes.card, `mb-6`,
+        <li className={classNames(classes.card, `mb-6 relative`,
         { [classes.cardHidden]: !category.includes(selectedTab) && selectedTab !== "all" })}>
-            <div className="h-full relative">
+            <div className={classNames(classes.content, "absolute h-full w-full")}>
                 <Image 
-                    alt={title}
+                    alt=""
                     layout="fill"
                     src={image}
                 />
+                <div className={classNames(classes.linkContainer, "absolute flex items-center justify-center w-full")}>
+                    <IconButton className="text-white hover:text-red-500">
+                        <LaunchIcon className="" />
+                    </IconButton>
+                </div>
+                <div className={classNames(classes.footer, `absolute bg-white bottom-0 flex flex-col items-center 
+                    justify-center w-full`)}>
+                    <Typography 
+                        component="h3"
+                        className="font-bold text-base text-gray-600">
+                        Modern design
+                    </Typography>
+                    <Typography 
+                        component="h4"
+                        className="capitalize font-bold text-xl">
+                        Art family residence
+                    </Typography>
+                </div>
             </div>
         </li>
     );
