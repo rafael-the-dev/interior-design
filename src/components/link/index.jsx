e58b77bd76f88e14437843237a4e291dd5c65dbc
Rafael Tivane <luislangabusiness@gmail.com>
2022-11-15 23:45:48 +0200

    update link component

--- src/components/link/index.jsx
+++ src/components/link/index.jsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 import { Typography } from "@mui/material";
 
 const LinkContainer = ({ className, children, href, onClick }) => (
-    <Link href={href} passHref>
-        <Typography 
-            className={className} component="a"
-            onClick={onClick}>
-            { children }
-        </Typography>
-    </Link>
+    <Typography 
+        className={className} 
+        component={Link}
+        onClick={onClick}>
+        { children }
+    </Typography>
 );
 
 export default LinkContainer;
