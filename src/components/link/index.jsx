0fffb14f2806571b5903037de54060985baf169f
Rafael Tivane <luislangabusiness@gmail.com>
2023-01-23 22:44:15 +0200

    [fix] React.Children.only expected to receive a single React element child.

--- src/components/link/index.jsx
+++ src/components/link/index.jsx
@@ -3,9 +3,13 @@ import { Typography } from "@mui/material";
 
 const LinkContainer = ({ className, children, href, onClick }) => (
     <Link href={href}
-            className={className} 
-            onClick={onClick}>
-            { children }
+        className={className} 
+        onClick={onClick}>
+        { children.length > 1 ? (
+            <div className={className}>
+                { children }
+            </div>
+        ) : children }
     </Link>
 );
 
