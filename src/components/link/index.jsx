401f7f0f2cc639e936e354510930af5028fe8e41
Rafael Tivane <luislangabusiness@gmail.com>
2023-01-23 22:59:44 +0200

    use Typography component in link

--- src/components/link/index.jsx
+++ src/components/link/index.jsx
@@ -2,15 +2,14 @@ import Link from "next/link";
 import { Typography } from "@mui/material";
 
 const LinkContainer = ({ className, children, href, onClick }) => (
-    <Link href={href}
+    <Typography 
+        component={Link} 
+        href={href}
         className={className} 
-        onClick={onClick}>
-        { children.length > 1 ? (
-            <div className={className}>
-                { children }
-            </div>
-        ) : children }
-    </Link>
+        onClick={onClick}
+        passHref>
+        { children }
+    </Typography>
 );
 
 export default LinkContainer;
